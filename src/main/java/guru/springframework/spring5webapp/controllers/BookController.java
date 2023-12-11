77b3028d36190e438344d866de54e16653cb36ab
mohaz3i <z3ifrimoha@gmail.com>
2023-12-11 20:46:25 +0100

    H2 console activated

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    @RequestMapping("/books")
+    public String getBooks(){
+
+        return "books";
+    }
+}
